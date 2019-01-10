9271a1219242b09a60ad4eb27dff1599c4bb7bc6
yangna <ityangna0402@163.com>
2019-01-10 10:49:05 +0800

    second distribution
    second distribution

---
+++ pytorch/pytorch_16.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 19:14:56 2019
+
+@author: natasha_yang
+
+@e-mail: ityangna0402@163.com
+"""
+
+####一些record
+pytorch是一个动态的框架而TensorFlow是一个静态的框架
+TensorFlow在GPU的分布式计算上更为出色,在数据量巨大时效率比pytorch要高一些
